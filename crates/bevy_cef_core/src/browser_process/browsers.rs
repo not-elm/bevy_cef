4ebfff2a25c9bb4413df6d297555c587fb537dc9
not-elm <elmgameinfo@gmail.com>
2025-10-26 01:55:05 +0900

    clippy

--- crates/bevy_cef_core/src/browser_process/browsers.rs
+++ crates/bevy_cef_core/src/browser_process/browsers.rs
@@ -10,11 +10,11 @@ use bevy_remote::BrpMessage;
 use cef::{
     Browser, BrowserHost, BrowserSettings, CefString, Client, CompositionUnderline,
     DictionaryValue, ImplBrowser, ImplBrowserHost, ImplDictionaryValue, ImplFrame, ImplListValue,
-    ImplProcessMessage, ImplRequestContext, MouseButtonType, PaintElementType, ProcessId, Range,
+    ImplProcessMessage, ImplRequestContext, MouseButtonType, ProcessId, Range,
     RequestContext, RequestContextSettings, WindowInfo, browser_host_create_browser_sync,
     dictionary_value_create, process_message_create,
 };
-use cef_dll_sys::{cef_event_flags_t, cef_mouse_button_type_t, cef_paint_element_type_t};
+use cef_dll_sys::{cef_event_flags_t, cef_mouse_button_type_t};
 #[allow(deprecated)]
 use raw_window_handle::RawWindowHandle;
 use std::cell::Cell;
